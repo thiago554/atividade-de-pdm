873e260ea1987d5a14dd01bdf128f0142f987adf
thiago554 <thiago.paulino@academico.ifpb.edu.br>
2026-04-15 10:58:30 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("teste.png")
-st.write("Site Dinaldo Jorge")
-st.link_button("Acessar", "https://www.dinaldo.com.br")
+st.image("thiago.jpeg")
+st.write("Site Thiago")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/sitethiago/in%C3%ADcio")
